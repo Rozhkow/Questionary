6ba604e80261f1d98eabf34c0dff42700bc4e434
Rozhkow <rozhkow.dimas@gmail.com>
2023-04-17 19:21:40 +0300

    feat(questionary): add first prototype

---
+++ components/FormHelper/FormHelper.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import { 
+  TextField, 
+  FormControl, 
+  FormControlLabel, 
+  Radio,
+  RadioGroup,
+  Checkbox
+} from '@material-ui/core';
+
+const TextQuestion = ({ question, answer, onAnswerChange }) => {
+  const handleInputChange = (event) => {
+    onAnswerChange(event.target.value);
+  };
+
+  return (
+    <div className="text-question">
+      <h2>{question}</h2>
+      <TextField value={answer || ''} onChange={handleInputChange} />
+    </div>
+  );
+};
+
+const RadioQuestion = ({ question, selectedAnswer, answers, onAnswerChange }) => {
+  const handleRadioChange = (event) => {
+    onAnswerChange(event.target.value);
+  };
+
+  return (
+    <div className="radio-question">
+      <h2>{question}</h2>
+      <FormControl component="fieldset">
+        <RadioGroup 
+          aria-label={question} 
+          name={question} 
+          value={selectedAnswer || ''} 
+          onChange={handleRadioChange}
+        >
+          {answers.map((answer) => (
+            <FormControlLabel 
+              key={answer} 
+              value={answer} 
+              control={<Radio />} 
+              label={answer} 
+            />
+          ))}
+        </RadioGroup>
+      </FormControl>
+    </div>
+  );
+};
+
+const LongTextQuestion = ({ question, answer, onAnswerChange }) => {
+  const handleInputChange = (event) => {
+    onAnswerChange(event.target.value);
+  };
+
+  return (
+    <div className="longtext-question">
+      <h2>{question}</h2>
+      <TextField
+        value={answer || ''}
+        onChange={handleInputChange}
+        multiline
+        rows={4}
+      />
+    </div>
+  );
+};
+
+const CheckboxQuestion = ({ question, options, selectedOptions, onOptionChange }) => {
+  const handleCheckboxChange = (event) => {
+    const optionIndex = parseInt(event.target.value);
+    const newSelectedOptions = [...selectedOptions];
+
+    if (event.target.checked) {
+      newSelectedOptions.push(optionIndex);
+    } else {
+      newSelectedOptions.splice(newSelectedOptions.indexOf(optionIndex), 1);
+    }
+
+    onOptionChange(newSelectedOptions);
+  };
+  
+  return (
+    <div className="checkbox-question">
+      <h2>{question}</h2>
+      <FormControl component="fieldset">
+        {options.map((option, index) => (
+          <FormControlLabel
+            key={index}
+            control={
+              <Checkbox
+                checked={selectedOptions?.includes(index)}
+                onChange={handleCheckboxChange}
+                value={index}
+              />
+            }
+            label={option}
+          />
+        ))}
+      </FormControl>
+    </div>
+  );
+};
+
+export {
+  CheckboxQuestion,
+  TextQuestion,
+  RadioQuestion,
+  LongTextQuestion
+};
--- pages/questionary.js
+++ pages/questionary.js
@@ -1,82 +1,217 @@
-import React from "react";
-import GridContainer from "/components/Grid/GridContainer.js";
-import GridItem from "/components/Grid/GridItem.js";
+import React, { useState } from "react";
 import Card from "/components/Card/Card.js";
-import LocationOn from "@material-ui/icons/LocationOn";
-import Carousel from "react-slick";
-import Header from "/components/Header/Header.js";
-import classNames from "classnames";
-import { makeStyles } from "@material-ui/core/styles";
-import questions from "../questions.json"
+import CardHeader from "/components/Card/CardHeader.js";
+import CardBody from "/components/Card/CardBody.js";
+import questionsData from "../questions.json"
+import {
+  CheckboxQuestion,
+  TextQuestion,
+  RadioQuestion,
+  LongTextQuestion
+} from "../components/FormHelper/FormHelper.js";
 
-import styles from "/styles/jss/nextjs-material-kit/pages/components.js";
+import { Button } from '@material-ui/core';
 
-const useStyles = makeStyles(styles);
+const graph = {
+  1: {
+    text: "Question 1",
+    options: ["First", "Second", "Third"],
+    conditions: {
+      "First": 2,
+      "Second": 3,
+      "Third": 4,
+    },
+    type: 'radio'
+  },
+  2: {
+    text: "Question 2",
+    options: ["Red", "Green", "Blue"],
+    conditions: {
+      "Red": 5,
+      "Green": 6
+    },
+    type: 'radio'
+  },
+  3: {
+    text: "Question 3",
+    options: ["Option 1", "Option 2"],
+    conditions: {
+      "Option 1": 8,
+      "Option 2": 9
+    },
+    type: 'radio'
+  },
+  4: {
+    text: "Question 4",
+    options: ["Option A", "Option B"],
+    conditions: {
+      "Option A": 9,
+      "Option B": 10
+    },
+    type: 'radio'
+  },
+  5: {
+    text: "Question 5",
+    options: ["True", "False"],
+    conditions: {
+      "True": 6,
+      "False": 11
+    },
+    type: 'radio'
+  },
+  6: {
+    text: "Question 6",
+    options: ["Option X", "Option Y"],
+    conditions: {
+      "Option X": 7,
+      "Option Y": 11
+    },
+    type: 'radio'
+  },
+  7: {
+    text: "Question 7",
+    options: ["Option" , "Option Q"],
+    conditions: {
+      "Option P": 11,
+      "Option Q": 11
+    },
+    type: 'radio'
+  },
+  8: {
+    text: "Question 8",
+    options: ["Option R", "Option S"],
+    conditions: {
+      "Option R": 10,
+      "Option S": 10
+    },
+    type: 'radio'
+  },
+  9: {
+    text: "Question 8",
+    options: ["Option R", "Option S"],
+    conditions: {
+      "Option R": 10,
+      "Option S": 10
+    },
+    type: 'radio'
+  },
+  10: {
+    text: "Question 8",
+    options: ["Option R", "Option S"],
+    conditions: {
+      "Option R": 11,
+      "Option S": 11
+    },
+    type: 'radio'
+  },
+  11: {
+    text: "The end"
+  },
+ };
 
-const Questionary = (props) => {
-    const classes = useStyles();
-    const { ...rest } = props;
-    const questionList = questions.questions
-    console.log(questionList)
+const Questionary = () => {
+  const [currentQuestionId, setCurrentQuestionId] = useState(1);
+  const [answers, setAnswers] = useState({});
 
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: false
-    };
+  const handleAnswer = (answer) => {
+    setAnswers({ ...answers, [currentQuestionId]: answer });
+  };
+  
+  const handlePrevious = () => {
+    const answerKeys = Object.keys(answers);
+    if (
+      answerKeys.includes(String(currentQuestionId)) &&
+      answerKeys[answerKeys.length - 1] !== currentQuestionId
+    ) {
+      const questionPosition = answerKeys.indexOf(String(currentQuestionId))
+      if (questionPosition) {
+        setCurrentQuestionId(answerKeys[questionPosition - 1])
+      }
+    } else {
+      setCurrentQuestionId(answerKeys.length)
+    }
+  };
 
-    return (
-      <div>
-        <Header
-            brand={questions.title}
-            fixed
-            color="primary"
-            changeColorOnScroll={{
-                height: 400,
-                color: "white"
-            }}
-            {...rest}
-        />
-        
-        <div className={classNames(classes.main)} style={{marginTop: '70px'}}>
-            <div className={classes.container}>
-                <GridContainer>
-                <GridItem xs={12} sm={12} md={12} className={classes.marginAuto}>
-                    <Card carousel>
-                    <Carousel {...settings}>
-                        {questionList.length && questionList.map(question => {
-                            return (
-                                <div>
-                                    <h1>{question.title}</h1>
-                                    <h4>{question.description}</h4>
-                                </div>
-                            )
-                        })}
+  const handleNext = () => {
+    const currentAnswer = answers[currentQuestionId];
+    const currentCondition = graph[currentQuestionId].conditions;
+    
+    setCurrentQuestionId(currentCondition[currentAnswer]);
+  };
 
-                        <div>
-                            <img
-                                src="/img/bg3.jpg"
-                                alt="Third slide"
-                                className="slick-image"
-                            />
-                            <div className="slick-caption">
-                                <h4>
-                                    <LocationOn className="slick-icons" />
-                                    Yellowstone National Park, United States
-                                </h4>
-                            </div>
-                        </div>
-                    </Carousel>
-                    </Card>
-                </GridItem>
-                </GridContainer>
-            </div>
-            </div>
-      </div>
-    );
-  }
+  const renderQuestion = () => {
+    const question = graph[currentQuestionId];
+    if (!question) {
+      return <div>Question not found</div>;
+    }
+    const { text, options } = question;
+
+    switch (question.type) {
+      case 'text':
+        return (
+          <TextQuestion
+            question={text}
+            answer={answers[currentQuestionId]}
+            onAnswerChange={handleAnswer}
+          />
+        );
+      case 'radio':
+        return (
+          <RadioQuestion
+            question={text}
+            answers={options}
+            selectedAnswer={answers[currentQuestionId]}
+            onAnswerChange={handleAnswer}
+          />
+        );
+      case 'longtext':
+        return (
+          <LongTextQuestion
+            question={text}
+            answer={answers[currentQuestionId]}
+            onAnswerChange={handleAnswer}
+          />
+        );
+      case 'checkboxes':
+        return (
+          <CheckboxQuestion
+            question={text}
+            options={options}
+            selectedOptions={answers[currentQuestionId]}
+            onOptionChange={handleAnswer}
+          />
+        );
+      default:
+        return <div>Question not found</div>;
+    }
+  };
+
+  return (
+    <Card>
+        <CardHeader title="Questionare" />
+        <CardBody>
+          {renderQuestion()}
+          <div>
+            <Button
+              color="primary"
+              disabled={currentQuestionId === 1} 
+              onClick={handlePrevious}
+            >
+              Previous
+            </Button>
+            
+            <Button 
+              color="primary"
+              disabled={!answers[currentQuestionId]} 
+              onClick={handleNext}
+            >
+              Next
+            </Button>
+          </div>
+        </CardBody>
+      </Card>
+  );
+
+};
   
 export default Questionary;
